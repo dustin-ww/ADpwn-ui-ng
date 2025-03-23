cd3637e00d54333598a6ca83a6182fe03f5d1eb5
dustin-ww <wickertdustin@gmail.com>
2025-03-23 10:51:47 +0100

    feat: rebuild deps from old ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/ui'],
+  css: [
+    '~/assets/css/main.css',
+    'v-network-graph/lib/style.css'
+  ],
+  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/icon'],
   devtools: { enabled: true },
   vite: {
     plugins: [
